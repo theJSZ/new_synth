e8ac0026058cba33b079302b8378afe44d018bcd
Jussi Rantala <jussi.v.rantala@helsinki.fi>
2025-01-24 09:03:52 +0200

    ImGui #includes working

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,11 @@
 #include "stk/SineWave.h"
 #include "stk/RtWvOut.h"
 
-// #include "SDL2/SDL.h"
+#include "SDL2/SDL.h"
+
+#include "imgui/imgui.h"
+#include "imgui/backends/imgui_impl_sdl2.h"
+#include "imgui/backends/imgui_impl_opengl3.h"
 
 #include "voice.h"
 
@@ -18,70 +22,70 @@ std::atomic<bool> running(true); // flag that is shared across threads
 
 using namespace stk;
 
-// int guiThread(Voice* voices[], int nVoices) {
-//     // Initialize SDL
-//     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-//         std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
-//         return 1;
-//     }
+int guiThread(Voice* voices[], int nVoices) {
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
 
-//     SDL_Window* window = SDL_CreateWindow(
-//         "Synthesizer",
-//         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, // x and y position
-//         800, 600,                                       // width and height
-//         SDL_WINDOW_SHOWN
-//     );
+    SDL_Window* window = SDL_CreateWindow(
+        "Synthesizer",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, // x and y position
+        800, 600,                                       // width and height
+        SDL_WINDOW_SHOWN
+    );
 
-//     if (window == nullptr) {
-//         std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
-//         SDL_QUIT;
-//         return 1;
-//     }
+    if (window == nullptr) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_QUIT;
+        return 1;
+    }
 
-//     SDL_Renderer* renderer = SDL_CreateRenderer(
-//         window,
-//         -1,
-//         SDL_RENDERER_ACCELERATED
-//     );
-//     if (renderer == nullptr) {
-//         std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
-//         SDL_DestroyWindow(window);
-//         SDL_QUIT;
-//         return 1;
-//     }
+    SDL_Renderer* renderer = SDL_CreateRenderer(
+        window,
+        -1,
+        SDL_RENDERER_ACCELERATED
+    );
+    if (renderer == nullptr) {
+        std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_QUIT;
+        return 1;
+    }
 
-//     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // r, g, b, alpha
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // r, g, b, alpha
 
-//     SDL_Event event;
+    SDL_Event event;
 
-//     // SDL main loop
-//     while (running.load()) {
-//         while (SDL_PollEvent(&event)) {
-//             if (event.type == SDL_QUIT) {
-//                 running.store(false);
-//             }
-//             else if (event.type == SDL_MOUSEBUTTONDOWN) {
-//                 int mouseX = event.motion.x;
-//                 for (int i = 0; i < nVoices; ++i) {
-//                    voices[i]->setFrequency(static_cast<double>(mouseX));
-//                 }
-//             }
-//         }
-//         // fill window with selected color
-//         SDL_RenderClear(renderer);
-//         // update window
-//         SDL_RenderPresent(renderer);
-//         // wait 16ms for approx 60 fps
-//         SDL_Delay(16);
-//     }
+    // SDL main loop
+    while (running.load()) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running.store(false);
+            }
+            else if (event.type == SDL_MOUSEBUTTONDOWN) {
+                int mouseX = event.motion.x;
+                for (int i = 0; i < nVoices; ++i) {
+                   voices[i]->setFrequency(static_cast<double>(mouseX));
+                }
+            }
+        }
+        // fill window with selected color
+        SDL_RenderClear(renderer);
+        // update window
+        SDL_RenderPresent(renderer);
+        // wait 16ms for approx 60 fps
+        SDL_Delay(16);
+    }
 
-//     // clean up
-//     SDL_DestroyRenderer(renderer);
-//     SDL_DestroyWindow(window);
-//     SDL_Quit();
+    // clean up
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
-//     return 0;
-// }
+    return 0;
+}
 
 void audioThread(Voice* voices[], int nVoices) {
     Stk::setSampleRate( 44100.0 );
